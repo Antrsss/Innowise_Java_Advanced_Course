9f56c59381f88c0b43b546a304942c709c806399
Antrsss <zgdasha2510@gmail.com>
2026-03-15 13:10:55 +0300

    Task 9: CI Pipeline

--- src/test/java/by/zgirskaya/advanced_course/integration/BaseIntegrationTest.java
+++ src/test/java/by/zgirskaya/advanced_course/integration/BaseIntegrationTest.java
@@ -4,30 +4,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Testcontainers
 @ActiveProfiles("test")
 public abstract class BaseIntegrationTest {
 
-  @Container
-  static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
-
-  @Container
-  static GenericContainer<?> redis = new GenericContainer<>("redis:7-alpine")
-      .withExposedPorts(6379);
-
   @DynamicPropertySource
   static void configureProperties(DynamicPropertyRegistry registry) {
-    registry.add("spring.datasource.url", postgres::getJdbcUrl);
-    registry.add("spring.datasource.username", postgres::getUsername);
-    registry.add("spring.datasource.password", postgres::getPassword);
+    registry.add("spring.datasource.url", () -> "jdbc:postgresql://localhost:5432/postgres");
+    registry.add("spring.datasource.username", () -> "postgres");
+    registry.add("spring.datasource.password", () -> "625100");
 
-    registry.add("spring.data.redis.host", redis::getHost);
-    registry.add("spring.data.redis.port", () -> redis.getMappedPort(6379));
+    registry.add("spring.data.redis.host", () -> "localhost");
+    registry.add("spring.data.redis.port", () -> 6379);
   }
 }
